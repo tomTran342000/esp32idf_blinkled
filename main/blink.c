1f5c780d1fbc0c7e9c1d1b0c7c48f064e1be299f
tran vu hoang <tranhoang342000@gmail.com>
2021-10-01 10:54:36 +0700

    second commit

--- main/blink.c
+++ main/blink.c
@@ -7,23 +7,29 @@
 #define BLINK_GPIO1 2
 #define BLINK_GPIO2 0
 
-void app_main(void)
-{
-    gpio_reset_pin(BLINK_GPIO1);
-    gpio_reset_pin(BLINK_GPIO2);
-    /* Set the GPIO as a push/pull output */
+int count = 0;
+
+void blink_led(void *led){
+    gpio_pad_select_gpio(BLINK_GPIO1);
+    gpio_pad_select_gpio(BLINK_GPIO2);
     gpio_set_direction(BLINK_GPIO1, GPIO_MODE_OUTPUT);
     gpio_set_direction(BLINK_GPIO2, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        printf("Turning off the LED\n");
-        gpio_set_level(BLINK_GPIO1, 0);
-        gpio_set_level(BLINK_GPIO2, 0);
+    
+    while (1)
+    {
+        gpio_set_level(BLINK_GPIO1,0);
+        gpio_set_level(BLINK_GPIO2,1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        printf("Turning on the LED\n");
-        gpio_set_level(BLINK_GPIO1, 1);
-        gpio_set_level(BLINK_GPIO2, 1);
+        count++;
+        gpio_set_level(BLINK_GPIO1,1);
+        gpio_set_level(BLINK_GPIO2,0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+    
+    
+}
+
+void app_main(void)
+{
+    xTaskCreate(&blink_led,"blink_led",512,NULL,5,NULL);
 }
